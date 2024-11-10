43a8d9dcbd263afab291a3732dfccb45e2a76af2
Hasan-2077 <hmdmahmudul@hawk.iit.edu>
2024-11-09 18:50:30 -0600

    adding script
    adding slide animation for images

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,11 @@
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const sliderContainer = document.querySelector('.slider-container');
     const slidesContainer = document.querySelector('.slides');
-    const images = document.querySelectorAll('.slides img');
-    
-    // Duplicate images for seamless infinite scrolling
+    const images = Array.from(slidesContainer.children);
+  
+    // Clone images and append to create a seamless loop
     images.forEach((image) => {
       const clone = image.cloneNode(true);
       slidesContainer.appendChild(clone);
     });
-  });
-  
+})
